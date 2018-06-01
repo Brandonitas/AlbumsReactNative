3e38b6ad79b30b1266c209eef4eb1f19dc07007d
Brandon <angiebrand10@hotmail.com>
2018-05-31 21:41:22 -0500

    Header component

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+//Import library for making component
+import React from 'react'
+import { Text } from 'react-native'
+
+//Make component
+
+const Header = () => {
+  return <Text>Albumns!</Text>;
+};
+
+//Make component available to other parts of the app
+export default Header;
